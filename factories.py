1269f88e4864dfe3db846d48757048ecfc5066fe
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-10-02 08:15:50 +0200

    Build new ISO images

--- factories.py
+++ factories.py
@@ -9,6 +9,7 @@ from twisted.python import log
 from twisted.internet import defer
 
 import buildbot
+import datetime
 import os.path
 import json
 
@@ -106,6 +107,68 @@ class ArchISOBuildFactory(util.BuildFactory):
                 name='clean up',
                 command=['sudo', 'rm', '-rf', 'work'],
                 workdir=os.path.join(self.workdir, 'livecd'),
+            ),
+            steps.ShellCommand(
+                name='remove old images',
+                command=['bash', '-c', 'find /repo/images/nightly -type f -mtime +7 -exec rm {} \;'],
+            )
+        ])
+
+
+class ImageBuildFactory(util.BuildFactory):
+    """
+    Build factory for ISO images.
+    """
+
+    def __init__(self, *args, **kwargs):
+        util.BuildFactory.__init__(self, *args, **kwargs)
+
+        today = datetime.date.today().strftime('%Y%m%d')
+        title = 'Liri OS'
+        product = 'lirios'
+        releasever = '28'
+        imgname = '%s-%s-x86_64' % (product, today)
+        isofilename = imgname + '.iso'
+        checksumfilename = imgname + '-CHECKSUM'
+        cacherootdir = '/var/cache/lic'
+        kspath = '/tmp/livecd.ks'
+
+        self.addSteps([
+            steps.ShellCommand(
+                name='install tools',
+                haltOnFailure=True,
+                command=['dnf', 'install', '-y', 'git', 'spin-kickstarts', 'pykickstart', 'livecd-tools'],
+            ),
+            steps.Git(
+                name='checkout sources',
+                codebase=util.Property('codebase'),
+                repourl=util.Property('repository'),
+                branch=util.Property('branch'),
+                mode='incremental',
+                submodules=True,
+                shallow=True,
+            ),
+            steps.ShellCommand(
+                name='ksflatten',
+                haltOnFailure=True,
+                command=['ksflatten', '--config=%s-livecd.ks' % product, '-o', kspath],
+            ),
+            steps.ShellCommand(
+                name='build image',
+                haltOnFailure=True,
+                command=['livecd-creator', '--releasever=' + releasever,
+                         '--config=' + kspath, '--fslabel=' + imgname,
+                         '--title', title, '--product=' + product,
+                         '--cache=%s/dnf' % cacherootdir],
+            ),
+            steps.ShellCommand(
+                name='checksum',
+                haltOnFailure=True,
+                command=['bash', '-c', 'sha256sum -b --tag %s > /repo/images/nightly/%s' % (isofilename, checksumfilename)],
+            ),
+            steps.ShellCommand(
+                name='move file',
+                command=['mv', isofilename, '/repo/images/nightly/'],
             ),
             steps.ShellCommand(
                 name='remove old images',
