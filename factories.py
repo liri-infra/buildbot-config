ac3ecd5ee900a0ee31064747fbef91de3d8ea1c7
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-10-04 22:56:16 +0200

    OSTree build factory
    Start working on OSTree.

--- factories.py
+++ factories.py
@@ -2,6 +2,7 @@
 # ex: set filetype=python:
 
 from buildbot.process.build import Build
+from buildbot.process import remotecommand
 from buildbot.plugins import *
 from buildbot.steps.worker import CompositeStepMixin
 from buildbot import locks
@@ -181,6 +182,58 @@ class ImageBuildFactory(util.BuildFactory):
                 name='remove old images',
                 command=['bash', '-c', 'find /repo/images/nightly -type f -mtime +7 -exec rm {} \;'],
             )
+        ])
+
+
+class OSTreeBuildStep(steps.BuildStep, CompositeStepMixin):
+    """
+    Creates the OSTree repo if needed.
+    """
+    def __init__(self, **kwargs):
+        steps.BuildStep.__init__(self, haltOnFailure=True, **kwargs)
+
+    @defer.inlineCallbacks
+    def run(self):
+        # Initialize OS tree
+        statCmd = remotecommand.RemoteCommand('stat', {'file': 'build-repo'})
+        yield self.runCommand(statCmd)
+        if statCmd.didFail():
+            mkdirCmd = remotecommand.RemoteCommand('mkdir', {'dir': 'build-repo'})
+            yield self.runCommand(mkdirCmd)
+            if mkdirCmd.didFail():
+                defer.returnValue(buildbot.process.results.FAILURE)
+            else:
+                cmd = ['ostree', 'init', '--repo=build-repo', '--mode=bare-user']
+                initCmd = remotecommand.RemoteCommand('shell', {'command': cmd})
+                defer.returnValue(self.convertResult(initCmd))
+        # Make tree
+        cmd = ['rpm-ostree', 'tree', '--repo=build-repo', '--cachedir=cache', 'lirios-unstable-desktop.json']
+        makeCmd = remotecommand.RemoteCommand('shell', {'command': cmd})
+        defer.returnValue(self.convertResult(makeCmd))
+
+
+class OSTreeFactory(util.BuildFactory):
+    """
+    Build factory for rpm-ostree OS trees.
+    """
+    def __init__(self, *args, **kwargs):
+        util.BuildFactory.__init__(self, *args, **kwargs)
+        self.addSteps([
+            steps.ShellCommand(
+                name='install tools',
+                haltOnFailure=True,
+                command=['dnf', 'install', '-y', 'git', 'rpm-ostree'],
+            ),
+            steps.Git(
+                name='checkout sources',
+                codebase=util.Property('codebase'),
+                repourl=util.Property('repository'),
+                branch=util.Property('branch'),
+                mode='incremental',
+                submodules=True,
+                shallow=True,
+            ),
+            OSTreeBuildStep(name='create OS tree'),
         ])
 
 
