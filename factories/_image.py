87dd6c05e94b05294bc4bd7a88915be9f49567a7
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2019-01-22 11:35:20 +0100

    Refresh date when building images
    Fix dates that were stuck to when Buildbot was restarted last time.

--- factories/_image.py
+++ factories/_image.py
@@ -2,12 +2,36 @@
 # ex: set filetype=python:
 
 from buildbot.plugins import util, steps
+from buildbot.steps import master, worker
+from buildbot.process.properties import Interpolate
 
+from twisted.internet import defer
+
+import buildbot
 import datetime
 
 __all__ = [
     'ImageBuildFactory',
 ]
+
+
+class ImagePropertiesStep(steps.BuildStep):
+    def __init__(self, **kwargs):
+        steps.BuildStep.__init__(self, **kwargs)
+        self.logEnviron = False
+
+    def run(self):
+        product = 'lirios'
+        arch = 'x86_64'
+        today = datetime.date.today().strftime('%Y%m%d')
+
+        self.setProperty('product', product, self.name, runtime=True)
+        self.setProperty('arch', arch, self.name, runtime=True)
+        self.setProperty('imgname', '{}-{}-{}'.format(product, today, arch), self.name, runtime=True)
+        self.setProperty('isofilename', '{}-{}-{}.iso'.format(product, today, arch), self.name, runtime=True)
+        self.setProperty('checksumfilename', '{}-{}-{}-CHECKSUM'.format(product, today, arch), self.name, runtime=True)
+
+        return buildbot.process.results.SUCCESS
 
 
 class ImageBuildFactory(util.BuildFactory):
@@ -18,15 +42,11 @@ class ImageBuildFactory(util.BuildFactory):
     def __init__(self, *args, **kwargs):
         util.BuildFactory.__init__(self, *args, **kwargs)
 
-        today = datetime.date.today().strftime('%Y%m%d')
         title = 'Liri OS'
-        product = 'lirios'
         releasever = '29'
-        imgname = '%s-%s-x86_64' % (product, today)
-        isofilename = imgname + '.iso'
-        checksumfilename = imgname + '-CHECKSUM'
 
         self.addSteps([
+            ImagePropertiesStep(name='set properties'),
             steps.ShellCommand(
                 name='install tools',
                 haltOnFailure=True,
@@ -44,27 +64,26 @@ class ImageBuildFactory(util.BuildFactory):
             steps.ShellCommand(
                 name='ksflatten',
                 haltOnFailure=True,
-                command=['ksflatten', '--config=%s-livecd.ks' % product, '-o', 'livecd.ks'],
+                command=['ksflatten', Interpolate('--config=%(prop:product)s-livecd.ks'), '-o', 'livecd.ks'],
             ),
-
             steps.ShellCommand(
                 name='build image',
                 haltOnFailure=True,
                 command=[
                     'livecd-creator', '--releasever=' + releasever,
-                    '--config=livecd.ks', '--fslabel=' + imgname,
-                    '--title', title, '--product=' + product,
+                    '--config=livecd.ks', Interpolate('--fslabel=%(prop:imgname)s'),
+                    '--title', title, Interpolate('--product=%(prop:product)s'),
                     '--cache=/build/cache'
                 ],
             ),
             steps.ShellCommand(
                 name='checksum',
                 haltOnFailure=True,
-                command=['bash', '-c', 'sha256sum -b --tag %s > /repo/images/nightly/%s' % (isofilename, checksumfilename)],
+                command=['bash', '-c', Interpolate('sha256sum -b --tag %(prop:isofilename)s > /repo/images/nightly/%(prop:checksumfilename)s')],
             ),
             steps.ShellCommand(
                 name='move file',
-                command=['mv', isofilename, '/repo/images/nightly/'],
+                command=['mv', Interpolate('%(prop:isofilename)s'), '/repo/images/nightly/'],
             ),
             steps.ShellCommand(
                 name='remove old images',
